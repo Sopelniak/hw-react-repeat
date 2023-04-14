8a9a524b4cece5f8bc39b90787087c10f816d281
Sopelniak <asopelnyak@gmail.com>
2023-04-14 16:38:13 +0300

    hw1 statistic add

--- src/pages/HomeWork1/HomeWork1.jsx
+++ src/pages/HomeWork1/HomeWork1.jsx
@@ -1,7 +1,10 @@
 import { Profile } from "./components/Profile/Profile";
 import { Statistics } from "./components/Statistics/Statistics";
 import user from "../../resources/user.json";
+import data from "../../resources/data.json";
 import css from "./HomeWork1.module.css";
+
+const title = "Upload stats";
 
 export const HomeWork1 = () => {
   return (
@@ -13,7 +16,7 @@ export const HomeWork1 = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics />
+      <Statistics title={title} stats={data} />
     </div>
   );
 };
